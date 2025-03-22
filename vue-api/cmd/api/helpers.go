15e2c4481d34b5d9752b442afd447fcf6e9ffcb0
kons77 <staneua@gmail.com>
2025-03-23 00:15:03 +0200

    errorJSON helper functions simplify handlers.go and use new helper functions

--- vue-api/cmd/api/helpers.go
+++ vue-api/cmd/api/helpers.go
@@ -20,13 +20,18 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data an
 
 	err = dec.Decode(&struct{}{}) // make sure there is only one json file
 	if err != io.EOF {
+		/*
+			The value io.EOF is just a constant, that says you've reached the end of the file. This is from the documentation for that constant:
+			In this check, we just make sure that we have read the entire JSON that we've received. If we get an error that is io.EOF,
+			then all is good -- there is nothing else to read. However, any other error means that something went wrong --
+			we have more data to read, and we shouldn't have (and don't want) any more.*/
 		return errors.New("body must have only a single json value")
 	}
 
 	return nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, r *http.Request, status int, data any, headers ...http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
 	out, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
@@ -45,4 +50,18 @@ func (app *application) writeJSON(w http.ResponseWriter, r *http.Request, status
 	}
 
 	return nil
+}
+
+func (app *application) errorJSON(w http.ResponseWriter, err error, status ...int) {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	app.writeJSON(w, statusCode, payload)
 }
