50e8a529120a5dde2a466898ec6d209a3fcb5190
kons77 <staneua@gmail.com>
2025-03-22 19:57:21 +0200

    add vue router and main route. create login component. add and use components from the previos section

--- vue-app1/src/App.vue
+++ vue-app1/src/App.vue
@@ -1,26 +1,25 @@
 <template>
   <Header />
-  <Body />
+
+  <div>
+    <router-view/>
+  </div>
   <Footer />
   
 </template>
 
 <script>
   import Header from "./components/Header.vue";
-  import Body from "./components/Body.vue";
   import Footer from "./components/Footer.vue";
 
   export default {
     name: 'App',
     components: {
       Header,
-      Body, 
       Footer,
     },
   }
 </script>
-
-
 
 <style>
 
