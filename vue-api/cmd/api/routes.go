5fca3ee48286f88c4a9e105042fa3a90887f20c3
kons77 <staneua@gmail.com>
2025-03-22 23:35:05 +0200

    Implement CORS in routes.go file Add NoSurf in middleware.go for future use

--- vue-api/cmd/api/routes.go
+++ vue-api/cmd/api/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 // routes generates our routes and attaches them to handlers, using the chi router
@@ -14,7 +15,14 @@ import (
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter() // mux from multiplex
 	mux.Use(middleware.Recoverer)
-
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 	// mux.Use(NoSurf)
 
 	mux.Get("/users/login", app.Login)
--- vue-api/go.mod
+++ vue-api/go.mod
@@ -4,4 +4,7 @@ go 1.24.1
 
 require github.com/go-chi/chi/v5 v5.2.1
 
-require github.com/justinas/nosurf v1.1.1 // indirect
+require (
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
