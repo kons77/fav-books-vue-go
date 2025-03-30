51ce177895584f2f138bea12205c776a4f3a95c9
kons77 <staneua@gmail.com>
2025-03-30 19:57:31 +0300

    extract authentication logic to a separate module

---
+++ vue-app1/src/components/security.js
@@ -0,0 +1,29 @@
+import { store } from './store.js'
+import router from './../router/index.js'
+
+
+// this is going to be used only when I need to make a fetch request that requires authentication
+let Secturity = {
+    // make sure user is authenticated 
+    requireToken: function() {
+        if (store.token === '') {
+            router.push("/login");
+            return false
+        }
+    },
+
+    // create request options and send them back 
+    requestOptions: function(payload) {
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        headers.append("Authorization", "Bearer " + store.token);
+
+        return {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: headers,
+        }
+    }
+}
+
+export default Secturity;
