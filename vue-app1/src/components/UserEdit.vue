f8929675a149ecbec80977dcd1bf985fbf9c7dc4
kons77 <staneua@gmail.com>
2025-03-30 23:57:44 +0300

    create form in UserEdit.vue component

--- vue-app1/src/components/UserEdit.vue
+++ vue-app1/src/components/UserEdit.vue
@@ -3,6 +3,69 @@
       <div class="row">
         <div class="col">
           <h1 class="mt-3">User</h1>
+          <hr>
+          <form-tag @UserEditEvent="submitHandler" name="userform" event="UserEditEvent">
+            <text-input
+              v-model="user.first_name"
+              type="text"
+              required="true"
+              label="First Name"
+              :value="user.first_name"
+              name="first-name"></text-input>
+            
+              <text-input
+              v-model="user.last_name"
+              type="text"
+              required="true"
+              label="Last Name"
+              :value="user.last_name"
+              name="last-name"></text-input>
+            
+              <text-input
+              v-model="user.email"
+              type="text"
+              required="true"
+              label="Email"
+              :value="user.email"
+              name="email"></text-input>
+
+            <text-input
+              v-if="this.user.id ===0"
+              v-model="user.password"
+              type="password"
+              required="true"
+              label="Pasword"
+              :value="user.password"
+              name="password"></text-input>
+
+            <text-input
+              v-else
+              v-model="user.password"
+              type="password"              
+              label="Pasword"
+              :value="user.password"
+              name="password"></text-input>
+
+            <text-input
+              v-if="this.user.id ===0"
+              v-model="user.password"
+              type="password"
+              required="true"
+              label="Confirm Pasword"
+              :value="user.password"
+              name="confirm-password"></text-input>
+
+            <text-input
+              v-else
+              v-model="user.password"
+              type="password"              
+              label="Confirm Pasword"
+              :value="user.password"
+              name="confirm-password"></text-input>
+
+            <hr>
+
+          </form-tag>
         </div>
       </div>
     </div>
@@ -10,10 +73,41 @@
 
   
 <script>
-/*import Security from './security.js'
-  export default {
-    beforeMount() {
-      Security.requireToken();
+import Security from './security.js'
+import FormTag from './forms/FormTag.vue'
+import TextInput from './forms/TextInput.vue'
+import notie from 'notie'
+
+export default {
+  beforeMount() {
+    // Security.requireToken();
+
+    if (parseInt(String(this.$route.params.userId), 10) > 0) {
+      // edit an existing user
+      // TODO get user from db 
+    } else {
+      // add a new user 
     }
-  }     */
+  }, 
+  data() {
+    return {
+      user: {
+        id: 0,
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+      }
+    }
+  },
+  components: {
+    'form-tag': FormTag,
+    'text-input': TextInput,
+  }, 
+  methods: {
+    submitHandler() {
+
+    }
+  },
+}     
 </script>
