1b78cdcf0fb9ef7d0e86fdd6157f9db164b30ff8
kons77 <staneua@gmail.com>
2025-04-06 19:18:11 +0300

    add genres to the books and filter by genre

--- fav-books-vue/src/components/Books.vue
+++ fav-books-vue/src/components/Books.vue
@@ -6,6 +6,16 @@
         </div>
 
         <hr>
+        <div class="filters text-center mb-3">
+          <span class="filter me-1" v-bind:class="{ active: currentFilter === 0 }" v-on:click="setFilter(0)">ALL</span>            
+          <span class="filter me-1" v-bind:class="{ active: currentFilter === 7 }" v-on:click="setFilter(7)">CLASSIC</span>
+          <span class="filter me-1" v-bind:class="{ active: currentFilter === 2 }" v-on:click="setFilter(2)">FANTASY</span>
+          <span class="filter me-1" v-bind:class="{ active: currentFilter === 6 }" v-on:click="setFilter(6)">HORROR</span>
+          <span class="filter me-1" v-bind:class="{ active: currentFilter === 4 }" v-on:click="setFilter(4)">THRILLER</span>
+          <span class="filter me-1" v-bind:class="{ active: currentFilter === 1 }" v-on:click="setFilter(1)">SCIENCE FICTION</span>
+        </div>
+
+        <hr>
 
         <div>
           <div class="card-group">
@@ -20,7 +30,10 @@
 
                   <div class="card-body text-center">
                     <h6 class="card-title">{{ b.title }}</h6>
-                    {{ b.author.author_name }}
+                    <span class="book-author">{{ b.author.author_name }}</span><br>
+                    <small class="text-muted book-genre" v-for="(g, index) in b.genres" v-bind:key="g.id">
+                      <em class="me-1">{{ g.genre_name }}<template v-if="index !== (b.genres.length - 1)">,</template></em>
+                    </small>
                   </div>
                 </div>
               </div>
@@ -63,7 +76,31 @@ export default {
     });
   }, 
   methods: {
-    
+    setFilter: function(filter)  {
+      this.currentFilter = filter;
+    },
   },
 }
 </script>
+
+<style scoped>
+.filter {
+  padding: 6px 6px;
+  cursor: pointer;
+  border-radius: 6px;
+  transition: all 0.35s;
+  border: 1px solid silver;
+}
+
+.filter.active {
+  background: lightgreen;
+}
+
+.filter.hover {
+  background: rgb(173, 18, 18);
+}
+
+.book-author, .book-genre {
+  font-size: 0.8em;
+}
+</style>
