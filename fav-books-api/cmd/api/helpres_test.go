118962bc37a0d845ff531a1b04c300a13d63940c
kons77 <staneua@gmail.com>
2025-04-13 13:35:51 +0300

    test response from errJSON

--- fav-books-api/cmd/api/helpres_test.go
+++ fav-books-api/cmd/api/helpres_test.go
@@ -69,6 +69,8 @@ func Test_errorJSON(t *testing.T) {
 		t.Error(err)
 	}
 
+	testJSONPayload(t, rr)
+
 	errSlice := []string{
 		"(SQLSTATE 23505)",
 		"(SQLSTATE 22001)",
@@ -80,5 +82,20 @@ func Test_errorJSON(t *testing.T) {
 		if customErr != nil {
 			t.Error(customErr)
 		}
+		testJSONPayload(t, rr)
+	}
+}
+
+// test a response
+func testJSONPayload(t *testing.T, rr *httptest.ResponseRecorder) {
+	var requestPayload jsonResponse
+	decoder := json.NewDecoder(rr.Body)
+	err := decoder.Decode(&requestPayload)
+	if err != nil {
+		t.Error("received error when decoding errorJSON payload:,", err)
+	}
+
+	if !requestPayload.Error {
+		t.Error("error set to true in response from errorJSON, and it should be set to true")
 	}
 }
